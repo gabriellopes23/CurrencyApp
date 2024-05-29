c37827ece4554cca9267e724b1e2fa3b2972726b
Gabriel Lopes <lopessantosgabriel@outlook.com>
2024-05-29 08:19:33 -0300

    Initial Commit

---
+++ CurrencyApp/CurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyAppApp.swift
+//  CurrencyApp
+//
+//  Created by Gabriel Lopes on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
